a71a2827aa52c98bb97f1c069177ad4fbc72ce0a
kopalg20 <kopalgarg2005@gmail.com>
2025-01-19 22:36:45 +0530

    Dark Mode at home screen

--- RecipeFinder/lib/home_screen.dart
+++ RecipeFinder/lib/home_screen.dart
@@ -1,32 +1,60 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget { //changes to stateful widget
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _switch = false; // default is light theme
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Recipe Finder")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Find Recipes Based on Ingredients",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Enter an ingredient and discover delicious recipes!",
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/input'),
-              child: Text("Get Started"),
+    return MaterialApp( //created material app to handle theme switching
+      theme: _switch ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Recipe Finder"),
+          actions: [ //create switch button
+            Row(
+              children: [
+                Text("Dark Mode"),
+                Switch(
+                  value: _switch,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _switch = newValue;
+                    });
+                  },
+                ),
+              ],
             ),
           ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Find Recipes Based on Ingredients",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Enter an ingredient and discover delicious recipes!",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/input'),
+                child: Text("Get Started"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
